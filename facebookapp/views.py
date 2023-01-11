c89bcef61788f88042e7089ad13366b7a34e6f7b
kuchimanchi <kuchimanchi.22@mountblue.tech>
2023-01-11 10:51:48 +0530

    formatted code, css in rem

--- facebookapp/views.py
+++ facebookapp/views.py
@@ -294,7 +294,7 @@ def postComment(request):
         comment = request.POST.get('comment')
         post = Post.objects.get(id=id)
         name = request.user.username
-        profileimg=request.user.profile.profileimg.url
+        profileimg = request.user.profile.profileimg.url
         profile_of_commenter = Profile.objects.get(ids=request.user.id)
         body = comment
         print(body, comment, id)
@@ -303,7 +303,7 @@ def postComment(request):
         name = request.user.username
         body = comment
         comments.save()
-        return JsonResponse({'comment': body, 'name': name,'profileimg':profileimg,})
+        return JsonResponse({'comment': body, 'name': name, 'profileimg': profileimg, })
 
 
 @login_required
@@ -382,10 +382,9 @@ def acceptrequest(request):
             senduser = User.objects.get(id=friendid)
             sender_friend_object.friendnames.add(request.user)
             accepted_friend_object.friendnames.add(senduser)
-            sender_friend_object.no_of_friends=sender_friend_object.no_of_friends+1
-            accepted_friend_object.no_of_friends=accepted_friend_object.no_of_friends+1
-            
-            
+            sender_friend_object.no_of_friends = sender_friend_object.no_of_friends+1
+            accepted_friend_object.no_of_friends = accepted_friend_object.no_of_friends+1
+
             sender_friend_object.save()
             accepted_friend_object.save()
 
@@ -459,9 +458,9 @@ def removefriend(request):
         senduser = User.objects.get(username=friendname)
         friendobjeject.friendnames.remove(request.user)
         adminobject.friendnames.remove(senduser)
-        
-        friendobjeject.no_of_friends=friendobjeject.no_of_friends-1
-        adminobject.no_of_friends=adminobject.no_of_friends-1
+
+        friendobjeject.no_of_friends = friendobjeject.no_of_friends-1
+        adminobject.no_of_friends = adminobject.no_of_friends-1
 
         friendobjeject.save()
         adminobject.save()
@@ -570,7 +569,7 @@ def follow(request):
 
         searchedbyid_user_object = User.objects.get(id=searchedbyid)
         searchedid_user_object = User.objects.get(id=searchedid)
-        
+
         if searchedbyid_user_object not in searchedid_object.followedby.all():
             print("not in ------ follow")
             searchedbyid_object.following.add(searchedid_user_object)
@@ -579,12 +578,12 @@ def follow(request):
             searchedid_object.save()
             searchedid_object.save()
             followers_count = searchedid_object.no_of_followers
-            return JsonResponse({'followers_count': followers_count,'msg':'Unfollow'})
-        
+            return JsonResponse({'followers_count': followers_count, 'msg': 'Unfollow'})
+
         else:
-            print("yes it is in ---------follow")        
+            print("yes it is in ---------follow")
             followers_count = searchedid_object.no_of_followers
-            return JsonResponse({'followers_count': followers_count,'msg':'Unfollow'})
+            return JsonResponse({'followers_count': followers_count, 'msg': 'Unfollow'})
 
 
 def unfollow(request):
@@ -600,19 +599,19 @@ def unfollow(request):
 
         searchedbyid_user_object = User.objects.get(id=searchedbyid)
         searchedid_user_object = User.objects.get(id=searchedid)
-        
+
         if searchedbyid_user_object in searchedid_object.followedby.all():
             print("not in ----------unfollow")
             searchedbyid_object.following.remove(searchedid_user_object)
             searchedid_object.followedby.remove(searchedbyid_user_object)
             searchedid_object.no_of_followers = searchedid_object.no_of_followers-1
-            
+
             searchedid_object.save()
             searchedid_object.save()
             followers_count = searchedid_object.no_of_followers
-            return JsonResponse({'followers_count': followers_count,'msg':'Follow'})
-        
+            return JsonResponse({'followers_count': followers_count, 'msg': 'Follow'})
+
         else:
             print("yes it is in ------unfollow")
             followers_count = searchedid_object.no_of_followers
-            return JsonResponse({'followers_count': followers_count,'msg':'Follow'})
+            return JsonResponse({'followers_count': followers_count, 'msg': 'Follow'})
